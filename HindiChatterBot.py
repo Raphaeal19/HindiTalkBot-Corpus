3b98598c89358e817015bbe4ad7939d3509ebe43
Raphaeal19 <ayushpathak2017.comp@mmcoe.edu.in>
2019-09-22 14:54:14 +0530

    Started using ListTrainer

--- HindiChatterBot.py
+++ HindiChatterBot.py
@@ -4,8 +4,8 @@ import os
 from googletrans import Translator
 import speech_recognition as sr
 from polyglot.transliteration import Transliterator
-import random
-from datetime import datetime
+from chatterbot.trainers import ListTrainer
+from pocketsphinx import LiveSpeech
 
 tr = Translator()
 r = sr.Recognizer()
@@ -16,11 +16,20 @@ def speak(audioString, langOp):
 	tts.save("audio_files/audio.mp3")
 	os.system("mpg321 audio_files/audio.mp3 -q")
 
+def botTrainer(name):
+	bot = ChatBot(name)
+	trainers = ListTrainer(bot)
+
+	for files in os.listdir("chatterbot_corpus/data/hindi"):
+		data = open("chatterbot_corpus/data/hindi/" + files, 'r').readlines()
+		trainers.train(data)
+	return bot
+
 def recordAudio():
 	with sr.Microphone() as source:
 		print("Say Something!")
 		speak(translateLang("I am listening", "hi"), "hi")
-		audio = r.listen(source, timeout=1, phrase_time_limit=10)
+		audio = r.listen(source)
 
 	data = ""
 	try:
@@ -52,6 +61,21 @@ def transliterationLang(textString):
 	data = trl.transliterate(textString)
 	print("transliterationLang: ",data)
 	return data
+'''
+name = input("What will be the name for your bot? ")
+myBot = botTrainer(name)
+input1 = translateLang("aap kaise hai", "hi")
+print("INPUT1: ", input1)
+reply = myBot.get_response(input1).text
+print("REPLY: ", reply)
+speak(translateLang(reply, "hi"),"hi")
+'''
+#speak(translateLang("namaskar", "en"), "en-us")
 
+name = input("What will be the name for your bot? ")
+myBot = botTrainer(name)
+audioString = recordAudio()
+input1 = translateLang(audioString, "hi")
+reply = myBot.get_response(input1)
+speak(reply, "hi")
 
-speak("hello good Microphone", "hi")
